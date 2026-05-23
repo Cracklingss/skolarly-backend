72398edaf4e0ac4988a90516be429395764815db
Smith Degamo <smithdegamox2@gmail.com>
2026-05-23 21:04:36 +0800

    frontend connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express';
 import routes from '@/routes';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { ENV } from '@/config/env';
 
 const app = express();
 
+app.use(cors({
+  origin: ENV.FRONTEND_URL,
+  credentials: true
+}));
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api', routes);
 
